28578d00cbccbec426836ab367a04a9d0484dff1
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-04-09 06:48:15 +0700

    Melanjutkan load

--- src/core/database/load.py
+++ src/core/database/load.py
@@ -2,5 +2,16 @@
 # Modul ini berisi fungsi yang berkaitan
 # dengan pemuatan data dari file csv
 
-def readData(path):
+from os import walk
+from os.path import isdir, isfile
+
+def readFile(path:str) -> str:
+    if(isfile(path)):
+        fileReader = open(path, "r")
+        return fileReader.read()
+    else:
+        print("ERROR : Input harus merupakan lokasi file")
+        return ""
+
+def loadDatabase(dir:str) -> dict:
     pass
