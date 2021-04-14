3ed379cde45d45e2022e6304346839c3dbc319f9
febryola <febryola0402@gmail.com>
2021-04-14 16:26:56 +0700

    memperbaiki buig

--- src/core/item/permintaan.py
+++ src/core/item/permintaan.py
@@ -7,22 +7,20 @@ from core.auth import isValidUser
 
 def mintaConsumable(username):
     consumabledata = getTable("consumable")
-    line = False
-    while not line:
-        id_item = input("Masukan ID item   : ")
-        jumlah_permintaan = int(input("Jumlah            : "))
-        tanggal_permintaan = input("Tanggal permintaan: ")
+    id_item = input("Masukan ID item   : ")
+    jumlah_permintaan = int(input("Jumlah            : "))
+    tanggal_permintaan = input("Tanggal permintaan: ")
 
-        for i in range(len(consumabledata)):
-            if (consumabledata[i][0] == id_item):
-                if (int(consumabledata[i][3])) > jumlah_permintaan:
-                    newConsumable = int(consumabledata[i][3])-(jumlah_permintaan)
-                    consumabledata[i][3]= newConsumable
-                    print()
-                    print("item " + str(consumabledata[i][1])+ " (x" + str(jumlah_permintaan) +") telah diambil!!!")
-                elif (int(consumabledata[i][3]))== jumlah_permintaan:
-                    newConsumable1 = int(consumabledata[i][3])
-                    consumabledata[i][3]= newConsumable1  
-                    print()
-                    print("item " + str(consumabledata[i][1])+ " (x" + str(jumlah_permintaan) +") telah diambil!!!")   
+    for i in range(len(consumabledata)):
+        if (consumabledata[i][0] == id_item):
+            if (int(consumabledata[i][2]))> jumlah_permintaan:
+                newConsumable = (int(consumabledata[i][2]))-(jumlah_permintaan)
+                (consumabledata[i][2])= newConsumable
+                print()
+                print("item " + str(consumabledata[i][1])+ " (x" + str(jumlah_permintaan) +") telah diambil!!!")
+            elif (int(consumabledata[i][2])) == jumlah_permintaan:
+                newConsumable1 = (consumabledata[i][2])
+                (consumabledata[i][2]) = newConsumable1  
+                print()
+                print("item " + str(consumabledata[i][1])+ " (x" + str(jumlah_permintaan) +") telah diambil!!!")
     pass
