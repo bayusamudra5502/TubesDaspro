b10373a53a4c6f2a4515a195eee15e482e814420
Alvandhyto Pradhana <alvandhyto@gmail.com>
2021-04-14 14:08:48 +0700

    Realisasi fungsi login

--- src/core/auth/login.py
+++ src/core/auth/login.py
@@ -8,4 +8,24 @@ from .password import isValidPassword
 
 def login() -> str:
     dataUser = getTable("user")
+
+    loggedin = False
+
+    while not loggedin:
+        username = input('Masukan username: ')
+        password = input('Masukan password: ')
+
+        for i in range(len(dataUser)):
+            if (username == dataUser['data'][i]['username']) and isValidPassword(password, dataUser['data'][i]['password']):
+                print('Halo', username + '!', 'Selamat datang di Kantong Ajaib.')
+                loggedin = True
+                break
+
+            elif (username == dataUser['data'][i]['username']) and (not isValidPassword(password, dataUser['data'][i]['password'])):
+                print('Halo', username + '!', 'Password yang kamu input salah.')
+                break
+
+            else:
+                print('Username tidak ditemukan.')
+                break
     pass
