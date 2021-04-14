7379b1b641ce32e851cbf34c19bb53815553852c
febryola <febryola0402@gmail.com>
2021-04-15 06:20:55 +0700

    Menyelesaikan item permintaan

--- src/core/item/permintaan.py
+++ src/core/item/permintaan.py
@@ -11,16 +11,16 @@ def mintaConsumable(username):
     jumlah_permintaan = int(input("Jumlah            : "))
     tanggal_permintaan = input("Tanggal permintaan: ")
 
-    for i in range(len(consumabledata)):
-        if (consumabledata[i][0] == id_item):
-            if (int(consumabledata[i][2]))> jumlah_permintaan:
-                newConsumable = (int(consumabledata[i][2]))-(jumlah_permintaan)
-                (consumabledata[i][2])= newConsumable
+    for i in range(int(consumabledata['row_number'])):
+        if (consumabledata['data'][i]['id'] == id_item):
+            if (int(consumabledata['data'][i]['jumlah']))> jumlah_permintaan:
+                newConsumable =  (int(consumabledata['data'][i]['jumlah']))-(jumlah_permintaan)
+                (consumabledata['data'][i]['jumlah']) = newConsumable
                 print()
-                print("item " + str(consumabledata[i][1])+ " (x" + str(jumlah_permintaan) +") telah diambil!!!")
-            elif (int(consumabledata[i][2])) == jumlah_permintaan:
-                newConsumable1 = (consumabledata[i][2])
-                (consumabledata[i][2]) = newConsumable1  
+                print("Item " + str(consumabledata['data'][i]['nama'])+ " (x" + str(jumlah_permintaan) +") telah diambil!!!")
+            elif  (int(consumabledata['data'][i]['jumlah'])) == jumlah_permintaan:
+                newConsumable1 =  (consumabledata['data'][i]['jumlah'])
+                (consumabledata['data'][i]['jumlah']) = newConsumable1  
                 print()
-                print("item " + str(consumabledata[i][1])+ " (x" + str(jumlah_permintaan) +") telah diambil!!!")
+                print("Item " + str(consumabledata['data'][i]['nama']) + " (x" + str(jumlah_permintaan) +") telah diambil!!!")
     pass
