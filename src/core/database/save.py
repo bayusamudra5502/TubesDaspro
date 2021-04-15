df258b0ad9586a6f4bd10c4a917a8d7eb6b3a65d
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-04-15 23:06:04 +0700

    Memperbaiki file yang saat di save tiba-tiba menghilang

--- src/core/database/save.py
+++ src/core/database/save.py
@@ -2,23 +2,28 @@
 # Modul ini merupakan implementasi dari fitur
 # Penyimpanan pada program ini.
 
+from typing import cast
 from .database import readDatabase, resetChanged
 from os.path import join, abspath, isdir, exists
-from os import W_OK, access
+from os import W_OK, access, mkdir
 
 def save(saveDir):
     """Fungsi ini akan menyimpan semua data pada database di
     folder saveDir dalam format csv. Jika proses berhasil,
     fungsi mengembalikan True"""
-
+    
     if not exists(saveDir):
-        print(f"ERROR: Lokasi '{saveDir}' tidak ditemukan.")
-        return False
-
+        print(f"ERROR: Lokasi '{saveDir}' tidak ditemukan. Akan dibuat folder baru.")
+        try:
+            mkdir(saveDir)
+        except Exception:
+            print(f"Tidak bisa membuat folder {saveDir}. Pastikan anda memiliki akses.")
+            return False
+    
     if not isdir(saveDir):
         print(f"ERROR: Lokasi '{saveDir}' bukan merupakan folder.")
         return False
-    
+
     if not access(saveDir, W_OK):
         print(f"ERROR: Lokasi '{saveDir}' tidak bisa ditulis. Pastikan anda memiliki akses.")
         return False
@@ -33,8 +38,8 @@ def save(saveDir):
         filePath = abspath(join(saveDir, fileName))
         tableData = database["data"][tableName]
 
-        if access(filePath, W_OK):
-            file = open(filePath, "w")
+        if (access(filePath, W_OK) or not exists(filePath)):
+            file = open(filePath, "w+")
             for j in range(tableData["col_number"]):
                 file.write(tableData["columnName"][j])
                 if j != (tableData["col_number"] - 1):
@@ -51,6 +56,7 @@ def save(saveDir):
                         file.write("\n")
             
             isChangePartial = True
+            file.close()
         else:
             isChangeApply = False
             print(f"ERROR: File '{filePath}' tidak bisa ditulis. Pastikan anda memiliki akses.")
