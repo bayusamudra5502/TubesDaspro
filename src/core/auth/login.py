c0015faa96ed713d900c4a960d207d99f678f715
Alvandhyto Pradhana <alvandhyto@gmail.com>
2021-04-16 02:05:26 +0700

    Pembaruan fungsi login dan register.

--- src/core/auth/login.py
+++ src/core/auth/login.py
@@ -3,10 +3,22 @@
 # pada program ini. Pada modul ini terdiri dari
 # Fungsi-fungsi yang berkaitan dengan login
 
+
+# PUSTAKA
 from core.database import getTable
 from .password import isValidPassword
 
 
+# KAMUS
+# module login()
+# Modul ini akan menerima input username dan password
+# yang kemudian divalidasi dengan data pada user.csv
+# Jika pengguna berhasil login, modul akan mereturn
+# username user sehingga modul-modul lain dapat
+# memvalidasi role pengguna
+
+
+# ALGORITMA
 def login() -> str:
     dataUser = getTable("user")
     errcode = 0
@@ -15,16 +27,19 @@ def login() -> str:
     password = input('Masukan password: ')
 
     for i in range(dataUser['row_number']):
+        # Login Benar
         if (username == dataUser['data'][i]['username']) and isValidPassword(password, dataUser['data'][i]['password']):
             print('Halo', username + '!', 'Selamat datang di Kantong Ajaib.')
             return username
 
+        # Password Salah
         elif (username == dataUser['data'][i]['username']) and (
         not isValidPassword(password, dataUser['data'][i]['password'])):
             errcode = 0
             print('Halo', username + '!', 'Password yang kamu input salah.')
             break
 
+        # Username tidak ditemukan
         else:
             errcode = 1
             continue
