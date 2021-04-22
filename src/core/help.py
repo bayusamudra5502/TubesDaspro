49767bb9c91830a742f348bc0cfceb0d10315509
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-04-22 18:37:14 +0700

    Merapihkan help

--- src/core/help.py
+++ src/core/help.py
@@ -3,8 +3,10 @@
 # yang berkaitan dengan fitur help (F16)
 
 from core.auth import isAdminRole, isValidUser
+from os import system
 
 def help(username):
+    system("clear || cls")
     print("""\033[32m
                                    __    __   _______  __       ______   
                                   |  |  |  | |   ____||  |     |   _  \  
@@ -19,53 +21,69 @@ def help(username):
     print("""
     1. Perintah "register"
        akses    : admin
-       fungsi   : untuk melakukan pendaftaran user baru 
+       fungsi   : untuk melakukan pendaftaran user baru
+
     2. Perintah "login"
        akses    : admin, user
        fungsi   : untuk melakukan login ke sistem
+
     3. Perintah "carirarity"
        akses    : admin, user
        fungsi   : untuk melakukan pencarian gadget berdasarkan raritynya
+
     4. Perintah "caritahun"
        akses    : admin, user
        fungsi   : untuk melakukan pencarian gadget berdasarkan tahun ditemukan
+
     5. Perintah "tambahitem"
        akses    : admin
        fungsi   : untuk melakukan penambahan item 
+
     6. Perintah "hapusitem"
        akses    : admin
        fungsi   : untuk melakukan penghapusan item
+
     7. Perintah "ubahjumlah"
        akses    : admin
        fungsi   : untuk mengubah jumlah gadget dan consumable yang terdapat di dalam sistem
+
     8. Perintah "pinjam"
        akses    : user
        fungsi   : untuk melakukan peminjaman gadget pada sistem
+
     9. Perintah "kembalikan"
        akses    : user
        fungsi   : untuk melakukan pengembalian pada gadget yang telah dipinjam kepada sistem
+
     10. Perintah "minta"
         akses    : user
         fungsi   : untuk melakukan permintaan consumable
+
     11. Perintah "riwayatambil"
         akses    : admin
         fungsi   : untuk melihat riwayat 5 permintaan consumable terbaru yang descending tanggal
+
     12. Perintah "riwayatpinjam"
         akses    : admin
         fungsi   : untuk melihat riwayat 5 peminjaman gadget terbaru yang descending tanggal
+
     13. Perintah "riwayatkembali"
         akses    : admin
         fungsi   : untuk melihat riwayat 5 pengembalian gadget terbaru yang descending tanggal
+
     14. Perintah "eksperimen"
         akses    : user
         fungsi   : untuk menggabungkan consumable menjadi consumable yang baru dengan rarity yang baru
+
     15. Perintah "help"
         akses    : admin, user
         fungsi   : * menampilkan petunjuk dan fitur-fitur yang ada pada sistem
                    * dapat diakses tanpa harus melakukan login terlebih dahulu
+
     16. Perintah "save"
         akses    : admin, user
         fungsi   : untuk melakukan penyimpanan data
+        
     17. Perintah "exit"
         akses    : admin, user
         fungsi   : untuk keluar dari sistem, pilihan:
