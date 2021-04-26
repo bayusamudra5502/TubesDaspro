353e96e1ee3eebe50ac0ed8b7efc10157ef3c4b1
Fadhil Kusuma Wardhana <82263529+fadhilwardhana@users.noreply.github.com>
2021-04-26 15:55:10 +0700

    memperbaiki fitur pengembalian

--- src/core/item/pengembalian.py
+++ src/core/item/pengembalian.py
@@ -39,7 +39,6 @@ def pengembalianGadget(username):
             else:
                 # Meminta input
                 if no_returned == False:
-<<<<<<< HEAD
                     print("Silahkan input data objek yang ingin dikembalikan")
                     nomor_pinjam = input("ID peminjaman : ")
                     validation_jumlah = False
@@ -53,24 +52,6 @@ def pengembalianGadget(username):
                             if notFound == False:
                                 jumlah_kembali = int(input("Masukan jumlah item yang dikembalikan : "))
                                 if jumlah_kembali > 0:
-=======
-                    print("Silahkan input data objek yang ingin dikembalikan:")
-                    nomor_pinjam = input("ID peminjaman : ")
-                    jumlah_kembali = int(input("Masukan jumlah item yang dikembalikan : "))
-                    tanggal_kembali = input("Tanggal pengembalian : ")
-                    validation_jumlah = False
-
-                    # Validasi dan modifikasi data
-                    if jumlah_kembali > 0:
-                        if isValidTanggal(tanggal_kembali):
-                            notFound = True
-                            id_item = ""
-
-                            for i in range(dataPinjamGadget["row_number"]):
-                                if(dataPinjamGadget["data"][i]["id"] == str(nomor_pinjam)):
-                                    notFound=False
-
->>>>>>> acfed677c5496db3a9afc7844c011c53eae11d67
                                     if(int(dataPinjamGadget["data"][i]["jumlah"]) - int(dataPinjamGadget["data"][i]["jumlah_kembali"]) > jumlah_kembali):
                                         validation_jumlah = True
 
@@ -85,7 +66,6 @@ def pengembalianGadget(username):
                                         dataPinjamGadget["data"][i]["is_returned"] = "TRUE"
                                         dataPinjamGadget["data"][i]["jumlah_kembali"] = \
                                             str(int(dataPinjamGadget["data"][i]["jumlah_kembali"]) + jumlah_kembali)
-<<<<<<< HEAD
                                 else:   #jumlah_kembali <= 0
                                     print()
                                     print("Jumlah pengembalian harus lebih besar dari nol")
@@ -147,60 +127,4 @@ def pengembalianGadget(username):
                     print("Tidak ada item yang perlu anda kembalikan")
         else:
             print()
-=======
-                                    
-
-                            if notFound:   #dataPinjamGadget["data"][i]["id"] != str(nomor_pinjam)
-                                print("Tidak ada item dengan nomor peminjaman tersebut")
-                            else:
-                                if validation_jumlah:
-                                    notFound1 = True
-
-                                    for i in range(dataGadget["row_number"]):
-                                        if(dataGadget["data"][i]["id"] == id_item):
-                                            # Update data gadget di db
-                                            notFound1 = False
-                                            newGadget = int(dataGadget["data"][i]["jumlah"]) + jumlah_kembali
-                                            dataGadget["data"][i]["jumlah"] = str(newGadget)
-
-                                            print()
-                                            print("Item "+ dataGadget['data'][i]['nama'] + " (x"+str(jumlah_kembali) + ") " + " telah dikembalikan!")
-
-                                            if getUserID(username):
-                                                lastID = "KMB-0"
-                                                nextIndex = dataKembaliGadget["row_number"]
-
-                                                if(dataKembaliGadget["row_number"] > 0):
-                                                    lastIndext = dataKembaliGadget["row_number"]-1
-                                                    lastID = dataKembaliGadget["data"][lastIndext]["id"]
-
-                                                id1 = (generateNextID(lastID))
-
-                                                dataKembaliGadget["data"][nextIndex] = \
-                                                    {
-                                                        "id": id1,
-                                                        "id_peminjaman": nomor_pinjam,
-                                                        "tanggal_pengembalian": tanggal_kembali,
-                                                        "jumlah": str(jumlah_kembali)
-                                                    }
-                                                
-                                                # Terapkan perubahan di db
-                                                applyChange(dataGadget, "gadget")
-                                                applyChange(dataPinjamGadget, "gadget_borrow_history")
-                                                applyChange(dataKembaliGadget, "gadget_return_history")
-
-                                    if notFound1:   # dataGadget["data"][i]["id"] != id_item
-                                        print("Item tersebut sudah tidak ada dalam database gadget")
-                                else:   # validation_jumlah = False
-                                    print()
-                                    print("Jumlah yang ingin dikembalikan melebihi jumlah peminjaman")
-                        else:
-                            print()
-                            print("Masukan tanggal tidak valid, silakan masukan format tanggal yang valid")
-                    else:   # jumlah_kembali <= 0
-                        print("Jumlah pengembalian harus lebih besar dari nol")
-                else:   # no_returned == True
-                    print("Tidak ada item yang perlu anda kembalikan")
-        else:
->>>>>>> acfed677c5496db3a9afc7844c011c53eae11d67
             print("Silakan lakukan login sebagai user untuk menjalankan perintah ini")
